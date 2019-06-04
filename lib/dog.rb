10ee5ccfb24614a8c9e263e52dda75d6ec96917f
tommaso auerbach <tsa302@nyu.edu>
2019-06-04 19:15:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,8 @@
 class Dog
 
-attr_reader :name
+def initialize(name)
+  @name = name
+end
 
-  def name=(dog_name)
-    this_dogs_name = dog_name
-  end
-
-  def name
-    this_dogs_name
-  end
 
 end
